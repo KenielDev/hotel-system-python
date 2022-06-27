633ac1e206316a4f8b7629e926faf1d489360525
KenielDev <103508290+KenielDev@users.noreply.github.com>
2022-06-27 17:24:34 -0300

    Funcao Cadastro alterações

--- projetofinal.py
+++ projetofinal.py
@@ -1,19 +1,28 @@
 
-banco = open("database.txt", "a")
+#Alerta caso o campo esteja vazio
+def funcaoAlert(variavel):
+    while(variavel == ""):
+      print("\033[0;31mEste é um campo obrigatório!\033[m")
+      variavel = input("Por favor preencha o campo acima: ")
+      return variavel
 
+#Cadastra o cliente
 def funcaoCadastra():
 
+  banco = open("database.txt", "a")
   clientes = list()
 
   insereCliente = input("Insira o nome do titular: ")
+  funcaoAlert(insereCliente)
 
-  while(insereCliente == ""):
-    print("\033[0;31mEste é um campo obrigatório!\033[m")
-    print(insereCliente)
-   
   inserteCpf = input("Insira o cpf do titular: ")
   numeroPessoas = input("Insira o número de pessoas: ")
+
   tipoQuarto = input("Insira o tipo do quarto: ")
+  while((tipoQuarto != "S") and (tipoQuarto != "s") and (tipoQuarto != "D") and (tipoQuarto != "d") and (tipoQuarto != "P") and (tipoQuarto != "p")):
+    print("\033[0;31mCampo incorreto!\033[m")
+    tipoQuarto = input("Os tipos de quartos são S, D ou P, por favor insira um destes: ")
+
   numeroDias = input("Insira o numero de dias da reserva: ")
 
   nPessoas = int(numeroPessoas)
@@ -21,15 +30,14 @@ def funcaoCadastra():
 
   #calcula o valor total da diaria
 
-  if tipoQuarto == "S":
+  if tipoQuarto == "S" or "s":
     valorQuarto = 100
 
-  elif tipoQuarto == "D":
+  elif tipoQuarto == "D" or "d":
     valorQuarto = 200
 
-  elif tipoQuarto == "P":
+  elif tipoQuarto == "P" or "p":
     valorQuarto = 300
-
 
   #calcula o valor total da diaria
 
@@ -41,10 +49,7 @@ def funcaoCadastra():
   banco.writelines("Nome: {}; CPF: {}; Número de pessoas hospedadas: {}; Tipo de quarto: {}; Dias de hospedagem: {}; Valor total: {}; Status: {}".format(insereCliente,inserteCpf,numeroPessoas,tipoQuarto,numeroDias,valor,status) + "\n")
   
   banco.close()
-
   
-
-
 while True:
 
   print("""              *** Menu ***
@@ -59,3 +64,4 @@ while True:
 
   if opcao == 1:
     funcaoCadastra()
+
